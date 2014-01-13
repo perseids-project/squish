a539af9759b0091d4ed7609bb6ee7147b2620e82
caesarfeta <adamtavares@gmail.com>
2014-01-13 16:39:47 -0500

    Making CSS squishing possible.

--- sqUI.sh
+++ sqUI.sh
@@ -12,13 +12,20 @@ squish() {
 	outName=$(basename "$list")
 	ext="${outName##*.}"
 	outName="${outName%.*}"
-	echo "Building ${outName}"
+	
+	# Get the type
+	typ="${outName##*.}"
+	outName="${outName%.*}"
 	
 	# Build the output file.
+	echo "Building ${outName}"
 	python lib/make.py $list $outName
-	lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --file=build/$outName.js > build/$outName.min.js
-	
-	# Check for compiler errors... This is clumsy but ehhh.
-	lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --info=errors --file=build/$outName.js
+	if [ $typ=="JS" ];
+	then
+		lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --file=build/$outName.js > build/$outName.min.js
+		
+		# Check for compiler errors... This is clumsy but ehhh.
+		lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --info=errors --file=build/$outName.js
+	fi
 }
 squish $1
