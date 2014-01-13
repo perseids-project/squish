6b56073c4de9242c3a4e982c6556133e9f9f6fbf
caesarfeta <adamtavares@gmail.com>
2014-01-13 16:14:00 -0500

    Check for compiler errors.  Not efficient but it works.

--- sqUI.sh
+++ sqUI.sh
@@ -5,16 +5,20 @@ squish() {
 	cd $DIR
 	
 	# Make the build directory
-	mkdir -p $DIR/build
+	mkdir -p build
 	
 	# Extract the output filename from the list.
 	list=$1
 	outName=$(basename "$list")
 	ext="${outName##*.}"
 	outName="${outName%.*}"
+	echo "Building ${outName}"
 	
 	# Build the output file.
 	python lib/make.py $list $outName
 	lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --file=build/$outName.js > build/$outName.min.js
+	
+	# Check for compiler errors... This is clumsy but ehhh.
+	lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --info=errors --file=build/$outName.js
 }
 squish $1
