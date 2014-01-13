75826a54b96a644657cef478f62c12051205a098
caesarfeta <adamtavares@gmail.com>
2014-01-13 15:10:42 -0500

    Used some shell scripting magic to make sqUI.sh more reliable / dummy proof.

--- sqUI.sh
+++ sqUI.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
-# Make the build directory
-mkdir build
-
-# Extract the output filename from the list.
-list=$1
-outName=$(basename "$list")
-ext="${outName##*.}"
-outName="${outName%.*}"
-
-# Build the output file.
-python lib/make.py $list $outName
-lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --file=build/$outName.js > build/$outName.min.js
+squish() {
+	# Get the current directory
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	cd $DIR
+	
+	# Make the build directory
+	mkdir -p $DIR/build
+	
+	# Extract the output filename from the list.
+	list=$1
+	outName=$(basename "$list")
+	ext="${outName##*.}"
+	outName="${outName%.*}"
+	
+	# Build the output file.
+	python lib/make.py $list $outName
+	lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --file=build/$outName.js > build/$outName.min.js
+}
+squish $1
