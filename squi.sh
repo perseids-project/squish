471fa40afe1dd294e80db8292d43e12bd3bac24d
caesarfeta <adamtavares@gmail.com>
2014-04-18 16:30:51 -0400

    Added comments.  Improved output.

--- squi.sh
+++ squi.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 squish() {
-    # Get the current directory
+    #-------------------------------------------------------------
+    #  Get the current directory
+    #-------------------------------------------------------------
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     cd $DIR
     
-    # Extract the output filename from the list.
+    #-------------------------------------------------------------
+    #  Extract the output filename from the list.
+    #-------------------------------------------------------------
     list="${1}"
     outName=$(basename "$list")
     
     ext="${outName##*.}"
     outName="${outName%.*}"
     
-    # Get the type
+    #-------------------------------------------------------------
+    #  Build the output file.
+    #-------------------------------------------------------------
+    echo "squi.sh -- Squishing ${outName}"
+    
+    #-------------------------------------------------------------
+    #  Get the output file type
+    #-------------------------------------------------------------
     typ="${outName##*.}"
     outName="${outName%.*}"
     
-    # Build the output file.
-    echo "--Building ${outName}"
+    #-------------------------------------------------------------
+    #  Squish them all together
+    #-------------------------------------------------------------
     python lib/make.py ../$list ../$outName $typ
+    
+    #-------------------------------------------------------------
+    #  Javascript files get minified
+    #-------------------------------------------------------------
     if [ $typ=="JS" ];
     then
+        echo "squi.sh -- Minifying ${outName}"
         lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --file=../$outName.js > ../$outName.min.js
-        # Check for compiler errors... This is clumsy but ehhh.
+        
+        #-------------------------------------------------------------
+        #  Check for compiler errors... This is clumsy but ehhh.
+        #-------------------------------------------------------------
         lib/closure-compiler-cli/closure_compiler_cli.py --level=simple --info=errors --file=../$outName.js
     fi
 }
